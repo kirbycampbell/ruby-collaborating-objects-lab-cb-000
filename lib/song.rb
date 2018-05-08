da029d894fd8c68daa438088379db63374c48288
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-08 18:55:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,16 +7,16 @@ attr_accessor :name, :artist
   end
 
 #Connects new song with Artist by finding it in Artist search
-  def artist_name=(name)
+  def artist(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
 
-#Creates an array of title and artist for setting
+#Creates an array of title and artist by spltting for setting
   def self.new_by_filename(file)
     data_array = file.chomp(".mp3").split(" - ")
     song = Song.new(data_array[1])
-    song.artist_name = data_array[0]
+    song.artist = data_array[0]
     song
   end
 

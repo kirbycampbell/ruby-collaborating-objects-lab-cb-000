ac9ce9411d69292aeb3ac95eaedf5d978a37b201
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-08 17:25:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,9 +9,9 @@ attr_accessor :name, :artist, :mp3importer
     @artist
   end
 
-  def self.new_by_filename
+  def self.new_by_filename(file)
     song = self.new
-    song.name = file.split(" - ")[1]
+    #song.name = file.split(" - ")[1]
     song
   end
 

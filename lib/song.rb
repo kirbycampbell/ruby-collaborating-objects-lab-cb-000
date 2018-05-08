3692883b9cac47f669f3bfa4db266096bcac7995
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-08 17:56:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,9 @@ attr_accessor :name, :artist
     @name = name
   end
 
-  def artist
-    @artist
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 
   def self.new_by_filename(file)
